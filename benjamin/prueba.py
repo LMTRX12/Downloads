8de401e796f828d5db6556ab514806d4423ff43a
bemjamin <be.delgadoc@duocuc.cl>
2026-04-14 16:47:54 -0400

    primer comit

---
+++ benjamin/prueba.py
@@ -0,0 +1,23 @@
+# print("ingrese 3 numeros")
+# n1=float(input("ingrese un numero"))
+# n2=float(input("ingrese otro numero"))
+# n3=float(input("ingrese otro numero"))
+# prom=(n1+n2+n3)/3
+# print("el promedio es", round(prom, 1))
+# if prom>=4:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")  
+print("el valor a apagar es")
+print("el valor a apagar es")
+print("el valor a apagar es")
+print("el valor a apagar es")
+
+op=int(input())
+if op==1:
+    print("el valor a pagar es")
+elif op==2:
+    print("el valor a pagar es")
+elif op==3:
+    print("el valor a pagar es")
+else:print("opcion invalida")
