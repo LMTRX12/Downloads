19fbf14271ef2b871d36bab0d2a0483b6a8550d7
benja <be.delgadoc@duocuc.cl>
2026-04-24 18:29:19 -0400

    ultimos ejercios dia viernes

---
+++ benjamin/match.py
@@ -0,0 +1,15 @@
+# print("1-opcion")
+# print("2-opcion")
+# print("3-opcion")
+
+# op=int(input("seleccione una opcion"))
+# match op:
+#     case 1:
+#         print("el valor a pagar" ,  )
+#     case 2:
+#         print("el valor a pagar")
+#     case 3:
+#         print("el valor a pagar")
+#     case :
+#         print("opcion invalida")
+
---
+++ benjamin/while.py
@@ -0,0 +1,5 @@
+#uso y eplicacion del white
+cont=1
+while cont<=3
+print("repeticion numero" , cont)
+cont
